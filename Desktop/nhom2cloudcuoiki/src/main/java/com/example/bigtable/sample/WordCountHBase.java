3473573318536d72cc15ef085b0746ddc50b2f14
huyhoangnguyen97 <huyhoangnguyen97@gmail.com>
2018-05-22 21:47:07 +0700

    Update WordCountHBase.java

--- Desktop/nhom2cloudcuoiki/src/main/java/com/example/bigtable/sample/WordCountHBase.java
+++ Desktop/nhom2cloudcuoiki/src/main/java/com/example/bigtable/sample/WordCountHBase.java
@@ -40,45 +40,41 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
-/**
- * Writes the same results of WordCount to a new table
- */
 public class WordCountHBase {
 
-  final static Log LOG = LogFactory.getLog(WordCountHBase.class);
-
   public static final byte[] COLUMN_FAMILY = "cf".getBytes();
+
   public static final byte[] COUNT_COLUMN_NAME = "count".getBytes();
 
   public static class TokenizerMapper extends
-      Mapper<Object, Text, ImmutableBytesWritable, IntWritable> {
+          Mapper<Object, Text, ImmutableBytesWritable, IntWritable> {
 
+    // giá trị là 1 khi gán cho mỗi từ
     private final static IntWritable one = new IntWritable(1);
 
     @Override
     public void map(Object key, Text value, Context context) throws IOException,
-        InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString());
+            InterruptedException {
+      tringTokenizer itr = new StringTokenizer(value.toString());
       ImmutableBytesWritable word = new ImmutableBytesWritable();
-      while (itr.hasMoreTokens()) {
+
+
         word.set(Bytes.toBytes(itr.nextToken()));
-        context.write(word, one);
+      
+      while (itr.hasMoreTokens()) {
+        String token = itr.nextToken();
+        if(token.length() >= 4) {
+          word.set(Bytes.toBytes(token));
+          // gán cặp key-value
+          context.write(word, one);
+        }
       }
     }
   }
 
+
   public static class MyTableReducer extends
-      TableReducer<ImmutableBytesWritable, IntWritable, ImmutableBytesWritable> {
-
-    @Override
-    public void reduce(ImmutableBytesWritable key, Iterable<IntWritable> values, Context context)
-        throws IOException, InterruptedException {
-      int sum = sum(values);
-      Put put = new Put(key.get());
-      put.addColumn(COLUMN_FAMILY, COUNT_COLUMN_NAME, Bytes.toBytes(sum));
-      context.write(null, put);
-    }
-
+          TableReducer<ImmutableBytesWritable, IntWritable, ImmutableBytesWritable> {
     public int sum(Iterable<IntWritable> values) {
       int i = 0;
       for (IntWritable val : values) {
@@ -86,18 +82,38 @@ public class WordCountHBase {
       }
       return i;
     }
+
+    @Override
+    public void reduce(ImmutableBytesWritable key, Iterable<IntWritable> values, Context context)
+            throws IOException, InterruptedException {
+
+      int sum = sum(values);
+
+      
+      Put put = new Put(key.get());
+
+           put.addColumn(COLUMN_FAMILY, COUNT_COLUMN_NAME, Bytes.toBytes(sum));
+
+      // ghi dữ liệu vào bảng
+      context.write(null, put);
+    }
+
   }
 
   public static void main(String[] args) throws Exception {
+
+    nối đến bigtable instance
     Configuration conf = HBaseConfiguration.create();
+
+    
     String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
     if (otherArgs.length < 2) {
-      System.err.println("Usage: wordcount-hbase <in> [<in>...] <table-name>");
+      System.err.println("\nLỗi cú pháp: wordcount-hbase <in> [<in>...] <table-name>\n");
       System.exit(2);
     }
+    Job job = Job.getInstance(conf, "Đếm từ khoá");
 
-    Job job = Job.getInstance(conf, "word count");
-
+    
     for (int i = 0; i < otherArgs.length - 1; ++i) {
       FileInputFormat.addInputPath(job, new Path(otherArgs[i]));
     }
@@ -107,15 +123,18 @@ public class WordCountHBase {
       CreateTable.createTable(tableName, conf,
           Collections.singletonList(Bytes.toString(COLUMN_FAMILY)));
     } catch (Exception e) {
-      LOG.error("Could not create the table.", e);
+      System.out.println("\nGặp lỗi trong quá trình tạo table trong HBase!\n");
     }
 
-    job.setJarByClass(WordCountHBase.class);
+        job.setJarByClass(WordCountHBase.class);
+
+    
     job.setMapperClass(TokenizerMapper.class);
-    job.setMapOutputValueClass(IntWritable.class);
 
-    TableMapReduceUtil.initTableReducerJob(tableName.getNameAsString(), MyTableReducer.class, job);
-
+của key: IntWritable
+    job.setMapOutputValueClass(IntWritable.class);    TableMapReduceUtil.initTableReducerJob(tableName.getNameAsString(), MyTableReducer.class, job);
     System.exit(job.waitForCompletion(true) ? 0 : 1);
+    System.out.println("\nKết quả đã được đưa vào table 'wordcount-result-group4' hoàn tất! Nếu bạn thấy thông báo này tức là chương trình đã hoàn thành!\n");
   }
 }
+
